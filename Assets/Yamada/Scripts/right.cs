5c2d4a14ca55cc2e9b6e4b417525bfdfd7780ffb
ネモ <s2310286.yamada.cgp@gmail.com>
2023-08-22 23:37:53 +0900

    シーン作成
    東西南北の４シーンを作成しました。

---
+++ Assets/Yamada/Scripts/right.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class right : MonoBehaviour
+{
+    public void SwitchScene()
+    {
+        //SceneManager.LoadScene("JumpTestScene", LoadSceneMode.Single);
+    }
+}
