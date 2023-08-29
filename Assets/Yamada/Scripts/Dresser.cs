c1450084df5ad0323f9327d14898da63284cad2b
ネモ <s2310286.yamada.cgp@gmail.com>
2023-08-30 00:52:15 +0900

    dresserのスクリプトの書き換え
    dresserのスクリプトを書き換えました

--- Assets/Yamada/Scripts/Dresser.cs
+++ Assets/Yamada/Scripts/Dresser.cs
@@ -8,7 +8,7 @@ public class Dresser : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        dresserPanel.SetActive(false);
+        //dresserPanel.SetActive(false);
     }
 
     // Update is called once per frame
