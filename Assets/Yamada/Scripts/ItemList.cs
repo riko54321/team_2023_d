3cd942ce92ca3cc832234fa4fe68251c3467e795
ネモ <s2310286.yamada.cgp@gmail.com>
2023-08-30 16:31:27 +0900

    アイテム入手時の処理(途中)
    アイテム入手時に入手したアイテムが消えるようにしました。

---
+++ Assets/Yamada/Scripts/ItemList.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemList : MonoBehaviour
+{
+    List<GameObject> itemList = new List<GameObject>();//GameObject型のListを定義
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
