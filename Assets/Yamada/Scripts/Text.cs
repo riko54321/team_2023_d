cba50550dc3efec872d417f7dc8b192cba82954c
ネモ <s2310286.yamada.cgp@gmail.com>
2023-09-10 00:37:43 +0900

    「これ以上アイテムを取得できません」のスクリプト(途中)
    3つアイテムを取得した際、「これ以上アイテムを取得できません」と表示させるスクリプトを書きましたがうまく動きません。

---
+++ Assets/Yamada/Scripts/Text.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public ItemList itemList;
+    
+    public void TextDisplay()
+    {   
+        if (itemList.itemCount >= 3)
+        {
+             this.gameObject.SetActive(true);
+        }
+
+    }
+}
